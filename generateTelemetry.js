3fd49d33fae81da6717f0d12ec0e9091ed25dcbe
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-03-28 01:11:21 +0700

    Menambahkan scraper rocket-simulator.com untuk mendapatkan data

---
+++ generateTelemetry.js
@@ -0,0 +1,41 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  console.log("> GO TO RSIM");
+  await page.goto("https://www.rocket-simulator.com/csimulator.php", {
+    timeout: 0,
+  });
+
+  console.log("> FORM SUBMIT");
+  await page.$eval('input[name="t1"]', (el) => (el.value = "101"));
+  await page.$eval('form[name="form1"]', (form) => form.submit());
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+  console.log("> IDLE");
+
+  page.on("error", () => {
+    console.log("Reload");
+    page.reload();
+  });
+
+  const data = await page.evaluate(() => {
+    const rows = document.querySelectorAll("#AutoNumber1 tr");
+
+    const arr = Array.from(rows, (row) => {
+      const columns = row.querySelectorAll("td");
+      return Array.from(columns, (column) => column.innerText);
+    });
+
+    arr.shift();
+
+    return arr;
+  });
+  console.log("> EVALUATE");
+
+  await browser.close();
+  console.log("> CLOSED");
+
+  console.log(data);
+})();
